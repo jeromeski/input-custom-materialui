c3d8616f00d714420211a714405341c27a749b4e
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2022-05-04 13:54:15 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Input from "./Input";
-import "./styles.css";
 import Textarea from "./Textarea";
+import "./styles.css";
+import {
+  FormControl,
+  FormInnerWrapper,
+  FormInput,
+  FormItem,
+  FormLabel
+} from "./styled-form-components";
 
 export default function App() {
   return (
     <React.Fragment>
       <Input />
       <Textarea />
+      <FormItem>
+        <FormControl>
+          <FormLabel>Hello</FormLabel>
+          <FormInnerWrapper>
+            <FormInput />
+          </FormInnerWrapper>
+        </FormControl>
+      </FormItem>
     </React.Fragment>
   );
 }
